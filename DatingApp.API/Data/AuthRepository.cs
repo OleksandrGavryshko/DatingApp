918277072ae8d858d3b0b64e4c368482af705e00
Oleksandr Gavryshko <sasha.gavryshko@gmail.com>
2019-11-11 14:56:35 +0100

    3.27 login method

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
@@ -13,15 +15,37 @@ namespace DatingApp.API.Data
 
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                        return false;
+                }
+            }
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
         {
             byte[] passwordHash, passwordSolt;
             CreatePasswordHash(password, out passwordHash, out passwordSolt);
+
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSolt;
 
@@ -33,7 +57,6 @@ namespace DatingApp.API.Data
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSolt)
         {
-
             using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 passwordSolt = hmac.Key;
@@ -41,9 +64,11 @@ namespace DatingApp.API.Data
             }
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if (await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+            return false;
         }
     }
 }

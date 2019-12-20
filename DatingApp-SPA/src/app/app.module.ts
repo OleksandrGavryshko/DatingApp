6c91ceaea5c65653a921b9fb0f8fac3b0f5cc3dc
Oleksandr Gavryshko <OGavryshko@opteam.pl>
2019-12-20 13:19:01 +0100

    47

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,18 +2,16 @@ import { AppComponent } from './app.component';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { NgModule } from '@angular/core';
-import { ValueComponent } from './value/value.component';
-import { from } from 'rxjs';
-import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { from } from 'rxjs';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       NavComponent,
       HomeComponent,
       RegisterComponent
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   model: any = {}
+  @Output() cancelRegister = new EventEmitter();
 
   constructor() { }
 
@@ -19,6 +20,7 @@ export class RegisterComponent implements OnInit {
   }
 
   cancel() {
+    this.cancelRegister.emit(false);
     console.log('canceled');
   }
 
